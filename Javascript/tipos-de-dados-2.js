c5e26ba4350545f6025730b58ca3fd37aa9671d1
renatodeluna <deluna.developer@gmail.com>
2018-03-12 03:31:49 -0300

    tipos de dados javascript 2

---
+++ Javascript/tipos-de-dados-2.js
@@ -0,0 +1,19 @@
+// Função : Ler o nome de uma pessoa, sua idade e o seu salário, mostrar essas informações
+// Autor : Renato de Luna
+// Data : 12/03/2018
+
+(function(){
+    
+    'use strict';
+    
+    var aray1 = ["Nome do usuário : ","Idade do usuário : ","Salário do usuário : "];
+    var aray2 = [];
+
+	for (var i = 0; i < aray1.length; i++) {
+		var dados = prompt(aray1[i]);
+        aray2.push(dados);
+        
+		console.log(aray1[i] + dados);
+    }
+
+})();
--- Javascript/tipos-de-dados.js
+++ Javascript/tipos-de-dados.js
@@ -1,9 +1,17 @@
 // Função : Ler o nome de uma pessoa, sua idade e o seu salário, mostrar essas informações
 // Autor : Renato de Luna
-// Data : 09/03/2018
+// Data : 11/03/2018
 
 (function(){
     
     'use strict';
     
+    var nome = prompt("Digite seu nome : ");
+    var idade = prompt("Digite sua idade : ");
+    var salario = prompt("Digite seu salário : ");
+
+    console.log("Seu nome é : " + nome);
+    console.log("Sua idade é : " + idade);
+    console.log("Seu saláro é : " + salario);
+
 })();
