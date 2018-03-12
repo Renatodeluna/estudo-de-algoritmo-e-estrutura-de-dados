0ad36408d333f46d7827e1f994abaa3d93155f1f
renatodeluna <deluna.developer@gmail.com>
2018-03-11 23:56:58 -0300

    tipos de dados js

---
+++ Javascript/tipos-de-dados.js
@@ -0,0 +1,9 @@
+// Função : Ler o nome de uma pessoa, sua idade e o seu salário, mostrar essas informações
+// Autor : Renato de Luna
+// Data : 09/03/2018
+
+(function(){
+    
+    'use strict';
+    
+})();
