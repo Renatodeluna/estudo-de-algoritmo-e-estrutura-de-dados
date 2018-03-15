7730b0e2e0fe9679a55a6b74e4d34b7955b7e1d4
renatodeluna <deluna.developer@gmail.com>
2018-03-15 01:31:09 -0300

    troca de valores C

--- C/troca-de-valores.c
+++ C/troca-de-valores.c
@@ -1,4 +1,3 @@
-algoritmo "trocadevalores"
 // Função : Duas variavéis (A e B) possuem valores distintos (A = 5 e B = 10),
 // crie uma algoritmo que armazene esses dois valores nessas duas variáveis, e
 // efetue a troca dos valores de forma que a variável A passe a possuir o valor
@@ -6,20 +5,17 @@ algoritmo "trocadevalores"
 // mostre os valores trocados.
 // Autor : Renato de Luna
 // Data : 14/03/2018
-// Seção de Declarações 
-var
-   a, b, c : Inteiro
-   
-inicio
-// Seção de Comandos
-   a <- 5
-   b <- 10
-   
-   c <- a
-   a <- b
-   b <- c
-   
-   escreval("Valor de A é : ", a)
-   escreval("Valor de B é : ", b)
 
-fimalgoritmo
+#include <stdio.h>
+
+main() {
+
+    int a = 5, b = 10, c;
+
+    c = a;
+    a = b;
+    b = c;
+
+    printf("\nO valor de A é : %d", a);
+    printf("\nO valor de B é : %d", b);
+}
