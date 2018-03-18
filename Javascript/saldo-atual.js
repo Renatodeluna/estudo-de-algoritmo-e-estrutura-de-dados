6f9329c53c22b307bb5932e6c1efaeb89ac0d71c
renatodeluna <deluna.developer@gmail.com>
2018-03-18 01:08:58 -0300

    saldo atual Javascript

---
+++ Javascript/saldo-atual.js
@@ -0,0 +1,20 @@
+// Função : Um cliente de um banco tem um saldo positiov de R% 500,00. Fazer
+// um algoritmo de leia um cheque que entrou e calcule o saldo, mostrando o
+// saldo na tela.
+// Autor : Renato de Luna
+// Data : 18/03/2018
+
+(function(){
+
+	'use strict';
+
+	var saldo, cheque, saldo_atual;
+	
+	cheque = parseFloat(prompt("Digite o valor do cheque : "));
+
+	saldo = 500;
+	saldo_atual = saldo + cheque;
+	
+	console.log("O seu valor atual é : " + saldo_atual);
+
+})();
