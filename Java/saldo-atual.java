f7543897aed0c03587b44457dc22745d4c3b6a1b
renatodeluna <deluna.developer@gmail.com>
2018-03-18 01:02:43 -0300

    saldo atual Java

---
+++ Java/saldo-atual.java
@@ -0,0 +1,26 @@
+/*
+ * Função : Um cliente de um banco tem um saldo positiov de R% 500,00. Fazer
+ * um algoritmo de leia um cheque que entrou e calcule o saldo, mostrando o
+ * saldo na tela.
+ * Autor : Renato de Luna
+ * Data : 18/03/2018
+ */
+package saldoatual;
+
+import javax.swing.JOptionPane;
+public class SaldoAtual {
+
+    public static void main(String[] args) {
+        
+        float saldo, cheque, saldo_atual;
+        
+        cheque = Float.parseFloat(JOptionPane.showInputDialog("Digite o valor do cheque : "));
+        
+        saldo = 500;
+        saldo_atual = saldo + cheque;
+        
+        System.out.println("O seu valor atual é : " + saldo_atual);
+        
+    }
+    
+}
