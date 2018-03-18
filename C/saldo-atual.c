3a75336efd2b217321df9af4715f63adc7df4592
renatodeluna <deluna.developer@gmail.com>
2018-03-18 00:45:35 -0300

    saldo atual C

---
+++ C/saldo-atual.c
@@ -0,0 +1,21 @@
+// Função : Um cliente de um banco tem um saldo positiov de R% 500,00. Fazer
+// um algoritmo de leia um cheque que entrou e calcule o saldo, mostrando o
+// saldo na tela.
+// Autor : Renato de Luna
+// Data : 18/03/2018
+
+#include <stdio.h>
+
+main() {
+
+    float saldo, cheque, saldo_atual;
+
+    printf("Digite o valor do cheque : ");
+    scanf("%f",&cheque);
+
+    saldo = 500;
+    saldo_atual = saldo + cheque;
+
+    printf("\nO seu saldo atual é : %f", saldo_atual);
+
+}
