143807fdda9f048ababc13956aa0265392e4cbc2
renatodeluna <deluna.developer@gmail.com>
2018-03-30 04:44:28 -0300

    avaliacao de prova C

--- C/avaliacao-de-prova.c
+++ C/avaliacao-de-prova.c
@@ -1,8 +1,9 @@
 // Função : O sitema de avaliação de determinada disciplina, é composto por três
 // provas. A primeira prova tem peso 2, a segunda tem peso 3 e a terceira prova
 // tem peso 5. Faça um algoritmo para calcular a média final de um aluno desta
-// disciplina se a média for maior ou igual a 6, mostre APROVADO, se não, mostre
-// REPROVADO.
+// disciplinae se a média for maior ou igual a 7, mostre APROVADO, se a nota for
+// mostre maior que 4 e menor que 7, mostre RECUPERAÇÃO, e se a nota for a 4,
+// mostre REPROVADO.
 // Autor : Renato de Luna
 // Data : 16/03/2018
 // Revisão : 27/03/2018
@@ -25,10 +26,11 @@ main() {
     prova3 = (prova3 * 5) / 10;
     media_final = prova1 + prova2 + prova3;
 
-    if (media_final >= 6)
+    if (media_final >= 7)
         printf("A média final é : %f aluno APROVADO", media_final);
+    else if (media_final >= 4)
+        printf("A média final é : %f aluno em RECUPERAÇÃO", media_final);
     else
         printf("A média final é : %f aluno REPROVADO", media_final);
-
 
 }
