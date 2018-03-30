7461d2dfdbcbce33ad5c997c4a382586232744c3
renatodeluna <deluna.developer@gmail.com>
2018-03-30 05:29:18 -0300

    avaliacao de prova Javascript

--- Javascript/avaliacao-de-prova.js
+++ Javascript/avaliacao-de-prova.js
@@ -1,8 +1,9 @@
 // Função : O sitema de avaliação de determinada disciplina, é composto por três
 // provas. A primeira prova tem peso 2, a segunda tem peso 3 e a terceira prova
 // tem peso 5. Faça um algoritmo para calcular a média final de um aluno desta
-// disciplina se a média for maior ou igual a 6, mostre APROVADO, se não, mostre
-// REPROVADO.
+// disciplinae se a média for maior ou igual a 7, mostre APROVADO, se a nota for
+// mostre maior que 4 e menor que 7, mostre RECUPERAÇÃO, e se a nota for a 4,
+// mostre REPROVADO.
 // Autor : Renato de Luna
 // Data : 16/03/2018
 // Revisão : 27/03/2018
@@ -22,9 +23,13 @@
 	prova3 = (prova3 * 5) / 10;
 	media_final = prova1 + prova2 + prova3;
 	
-	if (media_final >= 6) {
+	if (media_final >= 7) {
 		console.log("A média final é : " + media_final + " aluno APROVADO");
-	} else {
+	} 
+	else if (media_final >= 4) {
+		console.log("A média final é : " + media_final + " aluno em RECUPERAÇÃO");
+	}
+	else {
 		console.log("A média final é : " + media_final + " aluno REPROVADO");
 	}
 
